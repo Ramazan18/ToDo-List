6e867fc7c2517c4af1de572c0feab8b039802c54
ramazan <ramzik18@inbox.ru>
2023-01-23 03:07:36 +0600

    Todo app

--- src/components/todos.vue
+++ src/components/todos.vue
@@ -1,44 +1,62 @@
-<script lang="ts"> 
-    import axios from 'axios'
+<script> 
+import axios from 'axios'
 
-    export default {
-        name:'Todos',
-        data(){
-            return{
-                todos:[],
-                errors:[]
-            }
-        },
-        created(){
-            axios.get('https://dummyjson.com/todos')
-            .then(response =>{
-                this.todos = response.data
-                console.log(response)
-            })
-            .catch(e =>{
-                this.errors.push(e)
-            })
+export default {
+    name:'Todos',
+    data(){
+        return{
+            todos:[],
+            errors:[]
         }
+    },
+    created(){
+        axios.get('https://dummyjson.com/todos')
+        .then(response =>{
+            this.todos = response.data.todos
+            console.log(this.todos)
+        })
+        .catch(e =>{
+            this.errors.push(e)
+        })
     }
+}
 </script>
 
 <template>
 
-    <div class="todos-item" v-for="value in todos" :key="value.todos">
-        {{ value.todo }}
+<div class="todos-item" v-for="(value,index) in todos" :key="index">
+      <label>
+        <input type="checkbox" v-model="value.completed" />
+                    <span class="bubble"></span>
+      </label>
 
-					<label>
-						<input type="checkbox" v-model="todo.done" />
-                        <span class="bubble"></span>
-					</label>
+      <div class="todo-content">
+        <input type="text" v-model="value.todo" />
+      </div>
 
-					<div class="todo-content">
-						<input type="text" v-model="todo.content" />
-					</div>
-
-					<div class="actions">
-						<button class="delete" @click="removeTodo(todo)">Delete</button>
-					</div>
-				</div>
+      <div class="actions">
+        <button class="delete" @click="removeTodo(value)">Delete</button>
+      </div>
+    </div>
 
 </template>
+
+<style scoped>
+.todo-list .todo-item {
+	display: flex;
+	align-items: center;
+	background-color: #FFF;
+	padding: 1rem;
+	border-radius: 0.5rem;
+	box-shadow: var(--shadow);
+	margin-bottom: 1rem;
+}
+.todo-item .todo-content {
+	flex: 1 1 0%;
+}
+.todo-item .actions {
+	display: flex;
+	align-items: center;
+}
+</style>
+
